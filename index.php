0f7e04ec5692f99f3cb68a4539b38b19a5d2d69a
ness993 <n.horvat7@gmail.com>
2020-04-01 12:44:27 +0200

    Cleaning code

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
 
-session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
+
+//loda conf and halpers
+require_once(ROOT.DS.'config'.DS.'config.php');
+require_once(ROOT.DS.'app'.DS.'lib'.DS.'helpers'.DS.'functions.php');
+
+//autoload classes
+function autoloader($className)
+{
+    if(file_exists(ROOT.DS.'core'.DS.$className.'.php'))
+    {
+         require_once(ROOT.DS.'core'.DS.$className.'.php');
+    }elseif(file_exists(ROOT.DS.'app'.DS.'controllers'.DS.$className.'.php'))
+    {
+        require_once(ROOT.DS.'app'.DS.'controllers'.DS.$className.'.php');
+    }elseif(file_exists(ROOT.DS.'app'.DS.'models'.DS.$className.'.php'))
+    {
+        require_once(ROOT.DS.'app'.DS.'models'.DS.$className.'.php');
+    }
+}
+
+spl_autoload_register('autoloader');
+
+session_start();
+
+
 $url = isset($_SERVER['PATH_INFO'])? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
-require_once(ROOT.DS.'core'.DS.'bootstrap.php');
+
+//Routh  the  request
+
+Router::route($url);
